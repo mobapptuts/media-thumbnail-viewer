cf9722539e787ba43b195e42d16960e3f9315229
mobapptuts <nige.henshaw@gmail.com>
2016-05-31 16:36:01 +0900

    First git commit including read external storage runtime permission support

---
+++ app/src/main/java/com/mobapptut/mediathumbviewer/MediaThumbMainActivity.java
@@ -0,0 +1,54 @@
+package com.mobapptut.mediathumbviewer;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MediaThumbMainActivity extends AppCompatActivity {
+
+    private final static int READ_EXTERNAL_STORAGE_PERMMISSION_RESULT = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_media_thumb_main);
+
+        checkReadExternalStoragePermission();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode) {
+            case READ_EXTERNAL_STORAGE_PERMMISSION_RESULT:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //Call cursor loader
+                    Toast.makeText(this, "Now have access to view thumbs", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void checkReadExternalStoragePermission() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                // Start cursor loader
+            } else {
+                if(shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    Toast.makeText(this, "App needs to view thumbnails", Toast.LENGTH_SHORT).show();
+                }
+                requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                        READ_EXTERNAL_STORAGE_PERMMISSION_RESULT);
+            }
+        } else {
+            // Start cursor loader
+        }
+    }
+}
