8a8b62b70a8eeb104035172dde448bb4e26d74c9
mobapptuts <nige.henshaw@gmail.com>
2016-06-01 10:11:25 +0900

    Added recyclerview adapter

---
+++ app/src/main/java/com/mobapptut/mediathumbviewer/MediaStoreAdapter.java
@@ -0,0 +1,45 @@
+package com.mobapptut.mediathumbviewer;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by nigelhenshaw on 1/06/2016.
+ */
+public class MediaStoreAdapter extends RecyclerView.Adapter<MediaStoreAdapter.ViewHolder> {
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.media_image_view, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView mImageView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            mImageView = (ImageView) itemView.findViewById(R.id.mediastoreImageView);
+        }
+
+        public ImageView getImageView() {
+            return mImageView;
+        }
+    }
+}
