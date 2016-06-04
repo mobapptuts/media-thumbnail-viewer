0b6468d9d12fbc52cf63dab06c31516eb8d55d4f
mobapptuts <nige.henshaw@gmail.com>
2016-06-04 12:48:07 +0900

    Added cursor loader for MediaStore to main activity

--- app/src/main/java/com/mobapptut/mediathumbviewer/MediaThumbMainActivity.java
+++ app/src/main/java/com/mobapptut/mediathumbviewer/MediaThumbMainActivity.java
@@ -2,16 +2,23 @@ package com.mobapptut.mediathumbviewer;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Build;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.app.LoaderManager;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
-public class MediaThumbMainActivity extends AppCompatActivity {
+public class MediaThumbMainActivity extends AppCompatActivity
+    implements LoaderManager.LoaderCallbacks<Cursor>{
 
     private final static int READ_EXTERNAL_STORAGE_PERMMISSION_RESULT = 0;
+    private final static int MEDIASTORE_LOADER_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +34,8 @@ public class MediaThumbMainActivity extends AppCompatActivity {
             case READ_EXTERNAL_STORAGE_PERMMISSION_RESULT:
                 if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     //Call cursor loader
-                    Toast.makeText(this, "Now have access to view thumbs", Toast.LENGTH_SHORT).show();
+                    // Toast.makeText(this, "Now have access to view thumbs", Toast.LENGTH_SHORT).show();
+                    getSupportLoaderManager().initLoader(MEDIASTORE_LOADER_ID, null, this);
                 }
                 break;
             default:
@@ -40,6 +48,7 @@ public class MediaThumbMainActivity extends AppCompatActivity {
             if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) ==
                     PackageManager.PERMISSION_GRANTED) {
                 // Start cursor loader
+                getSupportLoaderManager().initLoader(MEDIASTORE_LOADER_ID, null, this);
             } else {
                 if(shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
                     Toast.makeText(this, "App needs to view thumbnails", Toast.LENGTH_SHORT).show();
@@ -49,6 +58,39 @@ public class MediaThumbMainActivity extends AppCompatActivity {
             }
         } else {
             // Start cursor loader
+            getSupportLoaderManager().initLoader(MEDIASTORE_LOADER_ID, null, this);
         }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] projection = {
+                MediaStore.Files.FileColumns._ID,
+                MediaStore.Files.FileColumns.DATE_ADDED,
+                MediaStore.Files.FileColumns.MEDIA_TYPE
+        };
+        String selection = MediaStore.Files.FileColumns.MEDIA_TYPE + "="
+                + MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE
+                + " OR "
+                + MediaStore.Files.FileColumns.MEDIA_TYPE + "="
+                + MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
+        return new CursorLoader(
+                this,
+                MediaStore.Files.getContentUri("external"),
+                projection,
+                selection,
+                null,
+                MediaStore.Files.FileColumns.DATE_ADDED + " DESC"
+        );
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
